efed34a890944d96ec27834fc47b2646acc62969
Sara M <sara.monzon.fdz@gmail.com>
2019-04-23 16:22:08 +0200

    Added execution permission, fixed path

--- sanger_crontab_script.sh
+++ sanger_crontab_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ########## Configuration settings  ########
-PATH_SANGER_FOLDER="/home/smonzon/Documents/desarrollo/sanger_script/sanger_seq"
+PATH_SANGER_FOLDER="/processing_Data/bioinfoshare/sanger_seq"
 REMOTE_SAMBA_SHARED_FOLDER="/processing_Data/sanger_seq_users"
 SANGER_SCRIPT="/home/smonzon/Documents/desarrollo/sanger_script/sanger_script.sh"
 PROCESSED_FILE_DIRECTORY="/home/smonzon/Documents/desarrollo/sanger_script"
